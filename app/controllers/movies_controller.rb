ec43eb9837b11a63201ae3f09a3ece49e8ba7fb1
nataly garcia <naty_garcioli@hotmail.com>
2021-04-29 21:01:40 -0500

    added method returned movies case insensitive

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      #@movies = Movie.where(title: params[:query])
+      @movies = Movie.where("title ILIKE ?", params[:query])
+    else
+      @movies = Movie.all
+    end
   end
 end
