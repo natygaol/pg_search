c1e03d3d983ee5001b11f6c2006b21b3aec1f81c
nataly garcia <naty_garcioli@hotmail.com>
2021-04-30 13:18:38 -0500

    query fixed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      #ILIKE makes the query case insensitive
-      @movies = Movie.where("title ILIKE :query ? OR syllabus ILIKE :query", query: params[:query])
+      sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
-  end
 end
