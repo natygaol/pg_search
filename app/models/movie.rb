58d28c353d75e2f2517240828783a0fd2dab6800
nataly garcia <naty_garcioli@hotmail.com>
2021-04-30 14:44:46 -0500

    :

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,9 @@ class Movie < ApplicationRecord
   # :search_by_title_and_synopsis is the method that you'll call when you do the search
     pg_search_scope :search_by_title_and_synopsis,
       against: [ :title, :synopsis ],  # those are the columns you're gonna use to search
+      associated_against: {
+          director: [ :first_name, :last_name ]
+        },
       using: {
         # @@        partial words allowed with prefix true
         tsearch: { prefix: true } # <-- now `superman batm` will return something!
