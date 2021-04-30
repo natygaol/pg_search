aeacbf511003a26d112d2db99972631d7934cd24
nataly garcia <naty_garcioli@hotmail.com>
2021-04-30 14:24:07 -0500

    fixed pg search method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = "title ILIKE :query OR synopsis ILIKE :query"
-      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
     else
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,13 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
   include PgSearch::Model
-  pg_search_scope :search_by_title_and_synopsis,
-    against: [ :title, :synopsis ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
+  # :search_by_title_and_synopsis is the method that you'll call when you do the search
+    pg_search_scope :search_by_title_and_synopsis,
+      against: [ :title, :synopsis ],  # those are the columns you're gonna use to search
+      using: {
+        # @@        partial words allowed with prefix true
+        tsearch: { prefix: true } # <-- now `superman batm` will return something!
+      }
+
 end
